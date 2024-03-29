7bdeb2c766fa3397e15c7ef4c11b9ceb0651d88c
okwhy <59335640+okwhy@users.noreply.github.com>
2024-03-29 11:44:14 +0400

    Initial commit

---
+++ src/main/java/com/example/testtest/Controller.java
@@ -0,0 +1,14 @@
+package com.example.testtest;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class Controller {
+    @GetMapping("/api/hello")
+    public String ss(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/example/testtest/TesttestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.testtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class TesttestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertEquals("aa","aa");
+    }
+
+}
